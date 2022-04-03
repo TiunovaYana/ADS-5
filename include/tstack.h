6e2ae4917ac584ded91b738a65d8e868943b6b20
TiunovaYana <99291404+TiunovaYana@users.noreply.github.com>
2022-04-03 20:22:07 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,11 +4,12 @@
 
 template<typename T, int size>
 class TStack {
-  private:
+
+   private:
      T arr[100];
      int top;
 
-  public:
+   public:
      TStack() : top{ -1 } {
      }
      bool isEmpty() {
